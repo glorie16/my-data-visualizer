9408bb2d90f6fe42915770065da4beac401d986a
glorie16 <gcmendoza@cpp.edu>
2025-02-01 21:56:47 -0800

    Update DataVisualizer.java

--- DataVisualizer.java
+++ DataVisualizer.java
@@ -13,6 +13,10 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+* Hello!
+**/
+
 public class DataVisualizer {
 	public static void main (String [] args) {	
 	Scanner scnr = new Scanner(System.in);
